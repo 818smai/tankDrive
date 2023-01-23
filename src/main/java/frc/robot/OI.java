a714c40d078cac4f10affe575a4ed150f3901d47
sami <119636215+818smai@users.noreply.github.com>
2023-01-23 17:44:34 -0500

    gkfkguiuiuiuiyt

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class OI {
+
+    Joystick joyLeft = new Joystick(Constants.JOYSTICK_PORT_LEFT);
+    Joystick joyRight = new Joystick(Constants.JOYSTICK_PORT_RIGHT);
+
+    public double getJoystickLeftY() {
+        return (Math.abs(joyLeft.getY()) < Constants.JOYSTICK_LEFT_DEADZONE_Y ? 0: joyLeft.getY());
+    }
+
+    public double getJoystickRightY() {
+        return(Math.abs(joyRight.getY()) < Constants.JOYSTICK_RIGHT_DEADZONE_Y ? 0: joyRight.getY());
+    }
+
+}
+
